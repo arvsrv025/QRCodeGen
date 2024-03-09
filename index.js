c3e1f3cde59adbe946dc8036a4d69419faac82d9
arvsrv025 <aranvsrivastav25@gmail.com>
2024-03-09 16:49:23 +0530

    project completed

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 // console.log("terminal is working");
 
+import inquirer from 'inquirer';
+import qr from 'qr-image';
+import fs from 'fs';
+
+
+inquirer
+  .prompt([
+    {message:"Type the URL that you want the QR for",name:"URL"}
+  ])
+  .then((answers) => {
+    const url=answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+    fs.writeFile('message.txt', url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+      }); 
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
